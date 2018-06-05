e27f2365b916a6f9f06c6f98f7b149320dfa5116
snavdeepsingh <snavdeepsingh@gmail.com>
2018-06-04 22:43:01 -0500

    added basic file structure

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+
+
+// Listen on port 3000
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+  });
